0735dd13033625f58977ad9a7abf3b5c554b05f3
Chinmayee Sawant <chinmayee.m.sawant@gmail.com>
2023-04-07 18:51:13 +0530

    final commit as per date 7th Apr 2023

--- main.py
+++ main.py
@@ -1,16 +1,28 @@
 import json
+import nltk
 import uvicorn
 import pandas as pd
 from consts import *
 from typing import List
 from datetime import datetime
 from pydantic import BaseModel
+from chatbot_test import calling_the_bot
 
 from fastapi import FastAPI, status
 from disorder_prediction import predict_model
 from fastapi.responses import JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
 from sentiment_analysis_pretrained import sentiment_vader
+
+
+try:
+    # nltk.download('all')
+    nltk.download('stopwords')
+    nltk.download('punkt')
+    nltk.download('wordnet')
+except Exception as e:
+    print(e)
+
 
 app = FastAPI()
 app.add_middleware(
@@ -56,6 +68,12 @@ class SentimentMessageRequestBody(BaseModel):
     timestamp: datetime
 
 
+@app.post("/get-advice")
+def get_advice(text: str):
+    response = calling_the_bot(text)
+    return JSONResponse({"result": response})
+
+
 @app.post("/get-mental-disorder-status", status_code=status.HTTP_200_OK)
 def get_mental_disorder_status(request_body: MentalDisorderRequestBody):
     request_body = request_body.dict()
@@ -70,8 +88,8 @@ def get_mental_disorder_status(request_body: MentalDisorderRequestBody):
 @app.post("/get-sentiment-analysis-status", status_code=status.HTTP_200_OK)
 def get_sentiment_analysis_report(request_body: List[SentimentMessageRequestBody]):
     final_result = {
-        'positive': 0,
-        'negative': 0,
+        "positive": 0,
+        "negative": 0,
         "neutral": 0,
     }
     overall_status = {
@@ -81,7 +99,7 @@ def get_sentiment_analysis_report(request_body: List[SentimentMessageRequestBody
     }
     for req_body in request_body:
         req_body = req_body.dict()
-        if req_body["is_bot"]:
+        if not req_body["is_bot"]:
             scores = sentiment_vader(req_body["message"])
             if scores[-2] >= 0.05:
                 overall_status["positive"] += 1
@@ -90,9 +108,15 @@ def get_sentiment_analysis_report(request_body: List[SentimentMessageRequestBody
             else:
                 overall_status["neutral"] += 1
     if sum(overall_status.values()) > 0:
-        final_result["positive"] = overall_status["positive"] / sum(overall_status.values())
-        final_result["negative"] = overall_status["negative"] / sum(overall_status.values())
-        final_result["neutral"] = overall_status["neutral"] / sum(overall_status.values())
+        final_result["positive"] = overall_status["positive"] / sum(
+            overall_status.values()
+        )
+        final_result["negative"] = overall_status["negative"] / sum(
+            overall_status.values()
+        )
+        final_result["neutral"] = overall_status["neutral"] / sum(
+            overall_status.values()
+        )
     return JSONResponse({"result": final_result})
 
 
